cf94b279405fd938a615c6193fa96ee755dbdc90
kendall <190708208@qq.com>
2019-07-20 14:04:35 +0800

    init project

---
+++ src/main/java/com/kendall/dota2dataservice/Dota2DataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kendall.dota2dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2DataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dota2DataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kendall/dota2dataservice/Dota2DataServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kendall.dota2dataservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dota2DataServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
